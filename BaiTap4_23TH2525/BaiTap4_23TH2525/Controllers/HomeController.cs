aa752845081b5b85cacc4d6c9f39c6a40b16814e
Bùi Nhật Trường <truong.bn.cc23cth@ntu.edu.vn>
2024-09-30 11:33:56 +0700

    upload Baitap4

---
+++ BaiTap4_23TH2525/BaiTap4_23TH2525/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap4_23TH2525.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Mô tả thông tin ứng dụng";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Thông tin liên hệ";
+
+            return View();
+        }
+    }
+}
