a0dfe40e79e8210759cdd82e38612c6cd52d6086
Bùi Nhật Trường <truong.bn.cc23cth@ntu.edu.vn>
2024-10-01 16:49:18 +0700

    update bai tap 6

---
+++ BaiTap6_23TH2525/BaiTap6_23TH2525/Controllers/PhongBans_23TH2525Controller.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BaiTap6_23TH2525;
+
+namespace BaiTap6_23TH2525.Controllers
+{
+    public class PhongBans_23TH2525Controller : Controller
+    {
+        private QLNV_23TH2525Entities db = new QLNV_23TH2525Entities();
+
+        // GET: PhongBans_23TH2525
+        public ActionResult Index()
+        {
+            return View(db.PhongBans.ToList());
+        }
+
+        // GET: PhongBans_23TH2525/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PhongBan phongBan = db.PhongBans.Find(id);
+            if (phongBan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(phongBan);
+        }
+
+        // GET: PhongBans_23TH2525/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PhongBans_23TH2525/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaPB,TenPB")] PhongBan phongBan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PhongBans.Add(phongBan);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(phongBan);
+        }
+
+        // GET: PhongBans_23TH2525/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PhongBan phongBan = db.PhongBans.Find(id);
+            if (phongBan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(phongBan);
+        }
+
+        // POST: PhongBans_23TH2525/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaPB,TenPB")] PhongBan phongBan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(phongBan).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(phongBan);
+        }
+
+        // GET: PhongBans_23TH2525/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PhongBan phongBan = db.PhongBans.Find(id);
+            if (phongBan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(phongBan);
+        }
+
+        // POST: PhongBans_23TH2525/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PhongBan phongBan = db.PhongBans.Find(id);
+            db.PhongBans.Remove(phongBan);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BaiTap6_23TH2525/BaiTap6_23TH2525/Controllers/TimKiem_23TH2525Controller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap6_23TH2525.Controllers
+{
+    public class TimKiem_23TH2525Controller : Controller
+    {
+        private QLNV_23TH2525Entities db = new QLNV_23TH2525Entities();
+
+        public ActionResult Index(string searchString, string gender, string department, decimal? minSalary, decimal? maxSalary)
+        {
+            var nhanViens = db.NhanViens.AsQueryable();
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                nhanViens = nhanViens.Where(s => s.HoNV.Contains(searchString) || s.TenNV.Contains(searchString));
+            }
+
+            if (!String.IsNullOrEmpty(gender))
+            {
+                nhanViens = nhanViens.Where(s => s.GioiTinh == gender);
+            }
+
+            if (!String.IsNullOrEmpty(department))
+            {
+                nhanViens = nhanViens.Where(s => s.PhongBan.TenPB.Contains(department));
+            }
+
+            if (minSalary.HasValue)
+            {
+                nhanViens = nhanViens.Where(s => s.Luong >= minSalary);
+            }
+
+            if (maxSalary.HasValue)
+            {
+                nhanViens = nhanViens.Where(s => s.Luong <= maxSalary);
+            }
+
+            return View(nhanViens.ToList());
+        }
+    }
+
+}
